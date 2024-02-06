960535a5a6cd031eac816020754b384158361655
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2024-02-06 14:58:54 -0600

    backend sends frontend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/', (req, res) => {
-  res.send('Helo World!')
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  // res.send('Helo World!');
 })
 
 app.listen(port, () => {
